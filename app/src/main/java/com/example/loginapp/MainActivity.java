97f300a9e5f973d441bec2f331cad1b573a82d67
nayleaA <naygdz306@gmail.com>
2023-02-09 15:31:51 -0600

    Diseño muestra dialog de procesando

--- app/src/main/java/com/example/loginapp/MainActivity.java
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -5,8 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+import android.app.ProgressDialog;
+
+//bibliotecas de voley para validacion
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
 
 
 import com.google.android.material.button.MaterialButton;
@@ -17,9 +29,9 @@ public class MainActivity extends AppCompatActivity {
     String u,c;
 
     //Animación de carga del webservice para ver que hace la aplicación
-   // ProgressDialog progressDialog;
+    ProgressDialog progressDialog;
     //Cuando mandamos petición, se debe enviar string con muchos elementos para ejecutar volley
-    //RequestQueue requestQueue;
+    RequestQueue requestQueue;
     //String httpURI="https://moveint.com/agenda/usuario.php";
 
     @Override
@@ -33,9 +45,11 @@ public class MainActivity extends AppCompatActivity {
        loginbtn=(MaterialButton) findViewById(R.id.buttonacceder);
 
         //Inicializar o enlazar a requestQueue y el progressDialog
-        //requestQueue= Volley.newRequestQueue(MainActivity.this);
+        requestQueue= Volley.newRequestQueue(MainActivity.this);
         //Indicar dónde se ejecutará progressdialog
-        //progressDialog=new ProgressDialog(MainActivity.this);
+        progressDialog=new ProgressDialog(MainActivity.this);
+
+
         //cuando se de click
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,7 +68,11 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Debes introducir los datos en los dos campos", Toast.LENGTH_LONG).show();
         }
         else{
-            //
+            //Mostramos el progressDialog
+            progressDialog.setMessage("Procesando...");
+            progressDialog.show();
+
+            //Aqui debe ir la validacion si es correcto o si es incorrecto
             Toast.makeText(getApplicationContext(), "correcto o incorrecto", Toast.LENGTH_LONG).show();
         }
     }
