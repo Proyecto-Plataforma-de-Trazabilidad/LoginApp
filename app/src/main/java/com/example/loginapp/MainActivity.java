f4728f2b4dc991171a69bcdceb0484a1708ea100
nayleaA <naygdz306@gmail.com>
2023-02-09 15:45:32 -0600

    agregando los eventos de validacion y host

--- app/src/main/java/com/example/loginapp/MainActivity.java
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -24,30 +24,30 @@ import java.util.Map;
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-    EditText username,passname;
+    EditText username, passname;
     MaterialButton loginbtn;
-    String u,c;
+    String u, c;
 
     //Animación de carga del webservice para ver que hace la aplicación
     ProgressDialog progressDialog;
     //Cuando mandamos petición, se debe enviar string con muchos elementos para ejecutar volley
     RequestQueue requestQueue;
-    //String httpURI="https://moveint.com/agenda/usuario.php";
+    String httpURI = "https://proyectoapejal.000webhostapp.com/agenda/usuario.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       username=(EditText) findViewById(R.id.username);
-       passname=(EditText) findViewById(R.id.passname);
+        username = (EditText) findViewById(R.id.username);
+        passname = (EditText) findViewById(R.id.passname);
 
-       loginbtn=(MaterialButton) findViewById(R.id.buttonacceder);
+        loginbtn = (MaterialButton) findViewById(R.id.buttonacceder);
 
         //Inicializar o enlazar a requestQueue y el progressDialog
-        requestQueue= Volley.newRequestQueue(MainActivity.this);
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
         //Indicar dónde se ejecutará progressdialog
-        progressDialog=new ProgressDialog(MainActivity.this);
+        progressDialog = new ProgressDialog(MainActivity.this);
 
 
         //cuando se de click
@@ -60,20 +60,45 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    private void Login(){
-        u=username.getText().toString();
-        c=passname.getText().toString();
-        if(u.isEmpty() || c.isEmpty()){
+
+    private void Login() {
+        u = username.getText().toString();
+        c = passname.getText().toString();
+        if (u.isEmpty() || c.isEmpty()) {
             //vacio
             Toast.makeText(getApplicationContext(), "Debes introducir los datos en los dos campos", Toast.LENGTH_LONG).show();
         }
-        else{
+        else {
             //Mostramos el progressDialog
             progressDialog.setMessage("Procesando...");
             progressDialog.show();
 
-            //Aqui debe ir la validacion si es correcto o si es incorrecto
-            Toast.makeText(getApplicationContext(), "correcto o incorrecto", Toast.LENGTH_LONG).show();
-        }
-    }
-}
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, httpURI, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    //Si es otro error raro, detenemos el progess
+                    progressDialog.dismiss();
+                    //Mostrar el error de Volley exacto a través de la librería
+                    Toast.makeText(getApplicationContext(), error.toString(),Toast.LENGTH_LONG).show();
+                }
+            })//fin string
+            {//inicio antes de map
+                protected Map<String,String> getParams(){
+                    Map<String, String> parametros=new HashMap<>();
+                    //Parámetros que se esperan en el webservice
+                    parametros.put("email",u);
+                    parametros.put("password",c);
+                    parametros.put("opcion","login");
+                    return parametros;
+                }
+            };//fin map
+            //requestQueue ejecute la cadena
+            requestQueue.add(stringRequest);
+        }//else
+    }//private
+}//main
