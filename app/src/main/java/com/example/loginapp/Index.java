b56f1ad1ea3395d173af0e637badc59f324efb7e
nayleaA <naygdz306@gmail.com>
2023-02-15 08:52:37 -0600

    Eventos botones

--- app/src/main/java/com/example/loginapp/Index.java
+++ app/src/main/java/com/example/loginapp/Index.java
@@ -3,12 +3,32 @@ package com.example.loginapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class Index extends AppCompatActivity {
 
+    MaterialButton btnmapa,btnconsul;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
-    }
-}
+
+        btnmapa= (MaterialButton) findViewById(R.id.btnmapa);
+        btnconsul= (MaterialButton) findViewById(R.id.btnconscat);
+
+        btnmapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });//fin evento boton btnmapa
+
+        btnconsul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });//fin evento btnconsul
+
+    }//oncreate
+}//main
