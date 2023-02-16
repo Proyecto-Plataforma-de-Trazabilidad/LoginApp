32c0784e2267317d7bf36c2f7b97b75d47fd61d1
nayleaA <naygdz306@gmail.com>
2023-02-16 15:41:30 -0600

    Mapa ya con todos los marcadores desde BD

--- app/src/main/java/com/example/loginapp/Mapa.java
+++ app/src/main/java/com/example/loginapp/Mapa.java
@@ -3,28 +3,125 @@ package com.example.loginapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Mapa extends AppCompatActivity implements OnMapReadyCallback {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Mapa extends AppCompatActivity implements OnMapReadyCallback , GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
+
+
+
+    ProgressDialog progressDialog;
+    RequestQueue requestQueue;
+
+    String httpURI="https://proyectoapejal.000webhostapp.com/agenda/marcadoresjson.php";
+
+    ArrayList<Marcadores> listaPuntos = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
 
+        requestQueue= Volley.newRequestQueue(Mapa.this);
+        //Indicar dónde se ejecutará progressdialog
+        progressDialog=new ProgressDialog(Mapa.this);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+    private void TraerMarcadores(){
+        progressDialog.setMessage("Cargando...");
+        progressDialog.show();
+
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, httpURI,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+                        try{
+                            JSONArray result=new JSONArray(response);
+                            listaPuntos = new ArrayList<>();
+                            //JSONObject obj = new JSONObject(response);
+
+                            if (result != null) {
+                                for (int i = 0; i < result.length(); i++) {
+                                    JSONObject jsonObject = result.getJSONObject(i);
+                                    Marcadores marcadores = new Marcadores(jsonObject);
+                                    listaPuntos.add(marcadores);
+                                }
+                                CargarPuntosAMapa();
+                            }
+                        }
+                        catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+                //Mostrar el error de Volley exacto a través de la librería
+                Toast.makeText(getApplicationContext(), error.toString(),Toast.LENGTH_LONG).show();
+            }
+        }){
+            protected Map<String,String> getParams(){
+                Map<String, String> parametros=new HashMap<>();
+                return parametros;
+            }
+        };
+        //requestQueue ejecute la cadena
+        requestQueue.add(stringRequest);
+    }
+
+    private void CargarPuntosAMapa(){
+        mMap.clear();
+        if (listaPuntos.size() > 0) {
+            for (int i = 0; i < listaPuntos.size(); i++) {
+
+                LatLng marker = new LatLng((Double.parseDouble(listaPuntos.get(i).getLat())), (Double.parseDouble(listaPuntos.get(i).getLon())));
+                mMap.addMarker(new MarkerOptions().position(marker).title(listaPuntos.get(i).getNombre()).snippet(listaPuntos.get(i).getDomicilio()));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(marker));
+
+            }
+        }
+        else {
+            Toast.makeText(this, "Lo sentimos, no tenemos agentes en la Unidad Seleccionada", Toast.LENGTH_SHORT).show();
+        }
+        mMap.setOnMarkerClickListener(this);
+        mMap.setMinZoomPreference(12.0f);
+        mMap.setMaxZoomPreference(20.0f);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
     }
 
 
@@ -33,8 +130,24 @@ public class Mapa extends AppCompatActivity implements OnMapReadyCallback {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng casanay = new LatLng(19, -103);
-        mMap.addMarker(new MarkerOptions().position(casanay).title("casa nay"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(casanay));
+        LatLng sydney = new LatLng(19.525772, -103.36180);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        TraerMarcadores();
     }
+
+    public boolean onMarkerClick(Marker marker) {
+        try {
+            String clickCount = (String) marker.getTag();
+            //  return true;
+
+        } catch (Exception ex) {
+            Marcadores info = new Marcadores();
+            info = (Marcadores) marker.getTag();
+
+        }
+        return false;
+    }
+
 }
