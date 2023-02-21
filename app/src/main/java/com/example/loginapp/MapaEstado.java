49ec9e796dc84410354a6dc220da72a7a03e773b
nayleaA <naygdz306@gmail.com>
2023-02-21 17:35:32 -0600

    agregado plantilla mapa y boton volver para la consulta estado

--- app/src/main/java/com/example/loginapp/MapaEstado.java
+++ app/src/main/java/com/example/loginapp/MapaEstado.java
@@ -2,13 +2,31 @@ package com.example.loginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MapaEstado extends AppCompatActivity {
+
+    MaterialButton btnregresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa_estado);
+
+        btnregresar= (MaterialButton) findViewById(R.id.btnregresar2);
+
+        btnregresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regresa= new Intent(MapaEstado.this,SpinnerEstado.class);
+                startActivity(regresa);
+            }
+        });
+
+
     }
 }
