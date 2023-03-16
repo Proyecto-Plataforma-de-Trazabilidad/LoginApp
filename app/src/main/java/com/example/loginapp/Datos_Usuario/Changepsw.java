5298947fd068b417e60c3c26d2b4fad9a305544c
nayleaA <naygdz306@gmail.com>
2023-03-16 16:17:56 -0600

    Diseño ChangePaswoord

---
+++ app/src/main/java/com/example/loginapp/Datos_Usuario/Changepsw.java
@@ -0,0 +1,16 @@
+package com.example.loginapp.Datos_Usuario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.loginapp.R;
+
+public class Changepsw extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_changepsw);
+    }
+}
--- app/src/main/java/com/example/loginapp/Datos_Usuario/Perfil.java
+++ app/src/main/java/com/example/loginapp/Datos_Usuario/Perfil.java
@@ -2,15 +2,31 @@ package com.example.loginapp.Datos_Usuario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.loginapp.Indexs.Index;
+import com.example.loginapp.MainActivity;
 import com.example.loginapp.R;
+import com.google.android.material.button.MaterialButton;
 
 public class Perfil extends AppCompatActivity {
-
+    MaterialButton editar,change;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
-    }
-}
+
+        change=findViewById(R.id.btnchange);
+
+        change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent change= new Intent(Perfil.this, Changepsw.class);
+                startActivity(change);
+            }
+        });
+
+    }//fin on create
+}//fin class
