3eb1a99c048017818d290c876c76cc369292f374
nayleaA <naygdz306@gmail.com>
2023-03-26 14:07:45 -0600

    Calndarios en diseño periodo

--- app/src/main/java/com/example/loginapp/Indexs/Movimientos/Productores/ConsultaOrdenesPeridoProductor.java
+++ app/src/main/java/com/example/loginapp/Indexs/Movimientos/Productores/ConsultaOrdenesPeridoProductor.java
@@ -1,16 +1,75 @@
 package com.example.loginapp.Indexs.Movimientos.Productores;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.example.loginapp.Base_Menu.DrawerBaseActivity;
 import com.example.loginapp.R;
+import com.example.loginapp.databinding.ActivityConsultaOrdenesPeridoProductorBinding;
+import com.example.loginapp.databinding.ActivityIndexMoviDistribuidorBinding;
 
-public class ConsultaOrdenesPeridoProductor extends AppCompatActivity {
+import java.nio.BufferUnderflowException;
+import java.time.Month;
+import java.util.Calendar;
 
-    @Override
+public class ConsultaOrdenesPeridoProductor extends DrawerBaseActivity {
+    ActivityConsultaOrdenesPeridoProductorBinding coppb;
+    EditText FI, FF;
+    int dia, mes, año;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consulta_ordenes_perido_productor);
+
+        //aqui va lo del menu
+        coppb= ActivityConsultaOrdenesPeridoProductorBinding.inflate(getLayoutInflater());
+        setContentView(coppb.getRoot());
+        allowActivityTitle("Movimientos/Ordenes/Periodo");
+
+        FI = findViewById(R.id.FI);
+        FF = findViewById(R.id.FF);
+        FI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrircalendario();
+            }
+        });
+        FF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrircalendario2();
+            }
+        });
     }
+
+    public void abrircalendario() {
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                // +1 because January is zero
+                final String selectedDate = day + " / " + (month + 1) + " / " + year;
+                FI.setText(selectedDate);//imprime en el cuadro
+            }
+        });
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void abrircalendario2(){
+        DatePickerFragment fragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                // +1 because January is zero
+                final String Date = day + " / " + (month+1) + " / " + year;
+                FF.setText(Date);//imprime en el cuadro
+            }
+        });
+        fragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
 }
