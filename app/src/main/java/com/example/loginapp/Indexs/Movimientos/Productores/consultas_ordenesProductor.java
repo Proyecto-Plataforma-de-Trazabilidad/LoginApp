225d908bdeef4160b9f44de7d24d9d857f925590
nayleaA <naygdz306@gmail.com>
2023-03-26 11:46:32 -0600

    enlazado a diseños productor

--- app/src/main/java/com/example/loginapp/Indexs/Movimientos/Productores/consultas_ordenesProductor.java
+++ app/src/main/java/com/example/loginapp/Indexs/Movimientos/Productores/consultas_ordenesProductor.java
@@ -1,8 +1,11 @@
 package com.example.loginapp.Indexs.Movimientos.Productores;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.loginapp.Base_Menu.DrawerBaseActivity;
 import com.example.loginapp.R;
@@ -11,6 +14,8 @@ import com.example.loginapp.databinding.ActivityIndexMoviDistribuidorBinding;
 
 public class consultas_ordenesProductor extends DrawerBaseActivity {
     ActivityConsultasOrdenesProductorBinding consultasOrdenesProductorBinding;
+    CardView G,P,TQ,TE;
+    Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,38 @@ public class consultas_ordenesProductor extends DrawerBaseActivity {
         consultasOrdenesProductorBinding= ActivityConsultasOrdenesProductorBinding.inflate(getLayoutInflater());
         setContentView(consultasOrdenesProductorBinding.getRoot());
         allowActivityTitle("Movimientos/Ordenes");
+
+        G=findViewById(R.id.tc1);
+        P=findViewById(R.id.tc2);
+        TQ=findViewById(R.id.tc3);
+        TE=findViewById(R.id.tc4);
+        G.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i=new Intent(consultas_ordenesProductor.this,ConsulGeneralDelProductor.class);
+                startActivity(i);
+            }
+        });
+        P.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i=new Intent(consultas_ordenesProductor.this,ConsultaOrdenesPeridoProductor.class);
+                startActivity(i);
+            }
+        });
+        TQ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i=new Intent(consultas_ordenesProductor.this,ConsultaOrdenesTipoQuimicoProductor.class);
+                startActivity(i);
+            }
+        });
+        TE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i=new Intent(consultas_ordenesProductor.this,consultaOrdenestipoembaProductor.class);
+                startActivity(i);
+            }
+        });
     }
 }
