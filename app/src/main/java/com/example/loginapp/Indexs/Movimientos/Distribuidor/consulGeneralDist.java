f02154e8be6035af6c89d3ec3a047070fa9e8ee0
Ever <86848042+Kirersays1@users.noreply.github.com>
2023-03-29 08:28:40 -0600

    ConsultasDist

---
+++ app/src/main/java/com/example/loginapp/Indexs/Movimientos/Distribuidor/consulGeneralDist.java
@@ -0,0 +1,16 @@
+package com.example.loginapp.Indexs.Movimientos.Distribuidor;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.loginapp.R;
+
+public class consulGeneralDist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consul_general_distri);
+    }
+}
