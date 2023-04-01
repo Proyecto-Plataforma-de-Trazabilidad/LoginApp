d65bb87cc6e6a7959b54b8d7fab4defa3447f8f9
Ever <86848042+Kirersays1@users.noreply.github.com>
2023-03-31 22:50:36 -0600

    Movimientos Distribuidor responsive
    Agregados los movimientos de distribuidor de manera responsive

--- app/src/main/java/com/example/loginapp/Indexs/Movimientos/Distribuidor/ConsultasOrdenesDistri.java
+++ app/src/main/java/com/example/loginapp/Indexs/Movimientos/Distribuidor/ConsultasOrdenesDistri.java
@@ -1,16 +1,65 @@
 package com.example.loginapp.Indexs.Movimientos.Distribuidor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.loginapp.Base_Menu.DrawerBaseActivity;
+import com.example.loginapp.Indexs.Movimientos.Distribuidor.Ordenes.consulGeneralDist;
+import com.example.loginapp.Indexs.Movimientos.Distribuidor.Ordenes.consultaOrdenEnvaseDistribuidor;
+import com.example.loginapp.Indexs.Movimientos.Distribuidor.Ordenes.ConsultaOrdenesQuimicoDist;
+import com.example.loginapp.Indexs.Movimientos.Distribuidor.Ordenes.ConsultaOrdenesPeridoDistribuidor;
 import com.example.loginapp.R;
+import com.example.loginapp.databinding.ActivityConsultasOrdenesDistriBinding;
 
-public class ConsultasOrdenesDistri extends AppCompatActivity {
+public class ConsultasOrdenesDistri extends DrawerBaseActivity{
+        ActivityConsultasOrdenesDistriBinding consultasOrdenesDistriBinding;
+        CardView G,P,TQ,TE;
+        Intent i;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_consultas_ordenes_distri);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_consultas_ordenes_distri);
+            //aqui va lo del menu
+            consultasOrdenesDistriBinding= ActivityConsultasOrdenesDistriBinding.inflate(getLayoutInflater());
+            setContentView(consultasOrdenesDistriBinding.getRoot());
+            allowActivityTitle("Movimientos/Ordenes");
+
+            G=findViewById(R.id.tc1);
+            P=findViewById(R.id.tc2);
+            TQ=findViewById(R.id.tc3);
+            TE=findViewById(R.id.tc4);
+            G.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    i=new Intent(ConsultasOrdenesDistri.this, consulGeneralDist.class);
+                    startActivity(i);
+                }
+            });
+            P.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    i=new Intent(ConsultasOrdenesDistri.this, ConsultaOrdenesPeridoDistribuidor.class);
+                    startActivity(i);
+                }
+            });
+            TQ.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    i=new Intent(ConsultasOrdenesDistri.this, ConsultaOrdenesQuimicoDist.class);
+                    startActivity(i);
+                }
+            });
+            TE.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    i=new Intent(ConsultasOrdenesDistri.this, consultaOrdenEnvaseDistribuidor.class);
+                    startActivity(i);
+                }
+            });
+        }
     }
-}
