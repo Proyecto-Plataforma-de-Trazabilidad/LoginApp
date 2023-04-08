54d723d457018695a06a78349751e1457ac6109e
nayleaA <naygdz306@gmail.com>
2023-04-08 14:06:09 -0600

    funciona cbotipo envase

--- app/src/main/java/com/example/loginapp/Indexs/Movimientos/Productores/Ordenes/consultaOrdenestipoembaProductor.java
+++ app/src/main/java/com/example/loginapp/Indexs/Movimientos/Productores/Ordenes/consultaOrdenestipoembaProductor.java
@@ -6,7 +6,11 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TableLayout;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
@@ -14,12 +18,15 @@ import com.example.loginapp.Indexs.Movimientos.Productores.consultas_ordenesProd
 import com.example.loginapp.R;
 import com.google.android.material.button.MaterialButton;
 
-public class consultaOrdenestipoembaProductor extends AppCompatActivity {
+public class consultaOrdenestipoembaProductor extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     TableLayout tbtOP,tbtdet;
     ProgressDialog progressDialog;
     RequestQueue requestQueue;
     String httpURI= "https://campolimpiojal.com/android/ConsulOrdenesMoviProductores.php";
     MaterialButton volver;
+    Spinner cboEnvase;
+    String[]Envases={"Rígidos lavables","Rígidos no lavables","Flexibles","Tapas","Cubetas","Cartón(Embalaje)","Tambos","Metal"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,5 +50,23 @@ public class consultaOrdenestipoembaProductor extends AppCompatActivity {
             }
         });
 
+        cboEnvase=findViewById(R.id.cboenvase);
+        ArrayAdapter<String> adapter=new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line,Envases);
+        cboEnvase.setAdapter(adapter);
+        cboEnvase.setOnItemSelectedListener(this);
+
+
     }//fin oncreate
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String value=parent.getItemAtPosition(position).toString();
+        Toast.makeText(this, "Rescate: "+value, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }//fin class
