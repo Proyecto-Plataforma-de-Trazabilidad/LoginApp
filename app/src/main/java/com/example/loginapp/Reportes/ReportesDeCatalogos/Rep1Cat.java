12730b6941d02c60a082f32a987c3788b0935049
nayleaA <naygdz306@gmail.com>
2023-05-14 14:49:06 -0600

    Reporte jalando al 100

--- app/src/main/java/com/example/loginapp/Reportes/ReportesDeCatalogos/Rep1Cat.java
+++ app/src/main/java/com/example/loginapp/Reportes/ReportesDeCatalogos/Rep1Cat.java
@@ -5,10 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -17,7 +13,6 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.loginapp.Movimientos.Productores.Extraviados.ConsultaExtraviadosProductor;
 import com.example.loginapp.R;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.Chart;
@@ -31,7 +26,6 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.DataSet;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
-import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
@@ -51,9 +45,11 @@ public class Rep1Cat extends AppCompatActivity implements OnChartValueSelectedLi
     String httpURI= "https://campolimpiojal.com/android/ConsuReportes.php";
     BarDataSet barDataSet;
 
-    BarChart barras2;
+    BarChart barras2,barras3;
     String[] etiquetas;
+    String[] etiquetasm;
     int[] valores;
+    int[] valoresm;
     int[] color=ColorTemplate.MATERIAL_COLORS;
 
 
@@ -231,7 +227,7 @@ public class Rep1Cat extends AppCompatActivity implements OnChartValueSelectedLi
     }
 
     private void creargrafico(){
-        barras2=(BarChart)getSameChart(barras2,"Cats",Color.BLACK,Color.WHITE,3000);
+        barras2=(BarChart)getSameChart(barras2,"Cats",Color.BLACK,Color.WHITE,2000);
         barras2.setDrawGridBackground(true);
 
         barras2.setData(getBarData());
@@ -264,8 +260,132 @@ public class Rep1Cat extends AppCompatActivity implements OnChartValueSelectedLi
 
     @Override
     public void onValueSelected(Entry e, Highlight h) {
-        Toast.makeText(Rep1Cat.this, "clickeaste en : "+etiquetas[(int) e.getX()], Toast.LENGTH_SHORT).show();
+        String barrac=etiquetas[(int) e.getX()];
+        Toast.makeText(this, barrac, Toast.LENGTH_SHORT).show();
+        ConsultaMunicipio(barrac);
+    }
 
+    private void ConsultaMunicipio(String barrac) {
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, httpURI, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressDialog.dismiss();
+                try {
+                    JSONArray result = new JSONArray(response);
+
+                    int ancho=result.length();
+
+                    etiquetasm=new String[ancho];
+                    valoresm=new int[ancho];
+
+                    for (int i = 0; i < result.length(); ) {
+                        JSONObject jsonObject = result.getJSONObject(i);
+
+                        int valor = Integer.parseInt(jsonObject.getString("TotalM"));
+                        String estado = jsonObject.getString("Municipio");
+                        etiquetasm[i]=estado;
+                        valoresm[i]=valor;
+                        i++;
+                    }
+                  graficoMunicipios(barrac);
+
+                }
+                catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+                //Mostrar el error de Volley exacto a través de la librería
+                Toast.makeText(getApplicationContext(), error.toString(),Toast.LENGTH_LONG).show();
+            }
+        }){
+            protected Map<String,String> getParams(){
+                Map<String, String> parametros=new HashMap<>();
+                parametros.put("opcion","RepMCat");
+                parametros.put("muni",barrac);
+                return parametros;
+            }
+        };
+        requestQueue.add(stringRequest);
+    }
+
+    private void graficoMunicipios(String barrac) {
+        barras3 = (BarChart) findViewById(R.id.CatM);
+        barras3=(BarChart)getSameChart3(barras3,"Municipios del Estado: "+barrac,Color.BLACK,Color.WHITE,2000);
+        barras3.setDrawGridBackground(true);
+
+        barras3.setData(getBarData3());
+        barras3.invalidate();
+
+        axisX3(barras3.getXAxis());//llamando asus propiedades personalizadas
+        axislefth3(barras3.getAxisLeft());
+        axisright3(barras3.getAxisRight());
+
+    }
+
+    private Chart getSameChart3(Chart chart,String des,int txtcolor,int back, int anima){
+        chart.getDescription().setText(des);//personalizacion de la grafica
+        chart.getDescription().setTextSize(15);
+        chart.setBackgroundColor(back);
+        chart.animateY(anima);
+        leyenda3(chart);
+        return chart;
+    }
+    private void leyenda3(Chart chart){
+        //personalizacion de la leyenda
+        Legend l=chart.getLegend();
+        l.setForm(Legend.LegendForm.CIRCLE);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+
+
+        ArrayList<LegendEntry>entriesm=new ArrayList<>();
+        for (int i = 0; i < etiquetasm.length; i++) {
+            LegendEntry entry=new LegendEntry();
+            entry.formColor=color[i];
+            entry.label= etiquetasm[i];
+            entriesm.add(entry);
+        }//fin for
+        l.setCustom(entriesm);
+    }
+
+    private void axisX3(XAxis axis){
+        axis.setGranularityEnabled(true);
+        axis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        axis.setValueFormatter(new IndexAxisValueFormatter(etiquetasm));
+        axis.setEnabled(false);//desabilita etiquetas eje x
+    }
+
+    private void axislefth3(YAxis axis){
+        axis.setSpaceTop(30);
+        axis.setAxisMinimum(0);
+    }
+
+    private void axisright3(YAxis axis){
+        axis.setEnabled(false);
+    }
+    private DataSet getData3(DataSet dataSet){
+        dataSet.setColors(color);
+        dataSet.setValueTextSize(10);
+        return dataSet;
+    }
+
+    private BarData getBarData3(){
+        BarDataSet bds= (BarDataSet) getData(new BarDataSet(Entriesgraf3(),""));//que datos vas a poner en la grafica
+        BarData bD=new BarData(bds);
+        bD.setBarWidth(0.45f);
+        return bD;
+    }
+    private ArrayList<BarEntry> Entriesgraf3(){
+        ArrayList<BarEntry> entries=new ArrayList<>();//agregar valores de la barra
+        for(int i=0;i<valoresm.length;){
+            entries.add(new BarEntry(i,valoresm[i]));
+            i++;
+        }
+        return entries;
     }
 
     @Override
