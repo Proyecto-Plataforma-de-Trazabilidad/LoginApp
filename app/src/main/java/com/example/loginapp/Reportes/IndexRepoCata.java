6a01498b94dc7e4596777376956a974eac5c3feb
nayleaA <naygdz306@gmail.com>
2023-05-14 18:42:13 -0600

    Reporte Distribuidor jalando al 100

--- app/src/main/java/com/example/loginapp/Reportes/IndexRepoCata.java
+++ app/src/main/java/com/example/loginapp/Reportes/IndexRepoCata.java
@@ -10,12 +10,13 @@ import android.view.View;
 import com.example.loginapp.Base_Menu.DrawerBaseActivity;
 import com.example.loginapp.R;
 import com.example.loginapp.Reportes.ReportesDeCatalogos.Rep1Cat;
+import com.example.loginapp.Reportes.ReportesDeCatalogos.Rep1Dis;
 import com.example.loginapp.databinding.ActivityIndexBinding;
 import com.example.loginapp.databinding.ActivityIndexRepoCataBinding;
 
 public class IndexRepoCata extends DrawerBaseActivity {
     ActivityIndexRepoCataBinding aircb;
-    CardView CAT;
+    CardView CAT,DIS;
     Intent i;
 
     @Override
@@ -29,10 +30,18 @@ public class IndexRepoCata extends DrawerBaseActivity {
         allowActivityTitle("Reportes/Catálogos");
 
         CAT=findViewById(R.id.T1);
+        DIS=findViewById(R.id.T2);
         CAT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 i=new Intent(IndexRepoCata.this, Rep1Cat.class);
+                startActivity(i);
+            }
+        });
+        DIS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i=new Intent(IndexRepoCata.this, Rep1Dis.class);
                 startActivity(i);
             }
         });
