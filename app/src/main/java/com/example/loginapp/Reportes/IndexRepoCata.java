414867692ea318045a6fdfb91576cdaa846aaeb0
nayleaA <naygdz306@gmail.com>
2023-05-14 19:49:51 -0600

    Reporte Productores jalando al 100

--- app/src/main/java/com/example/loginapp/Reportes/IndexRepoCata.java
+++ app/src/main/java/com/example/loginapp/Reportes/IndexRepoCata.java
@@ -11,12 +11,13 @@ import com.example.loginapp.Base_Menu.DrawerBaseActivity;
 import com.example.loginapp.R;
 import com.example.loginapp.Reportes.ReportesDeCatalogos.Rep1Cat;
 import com.example.loginapp.Reportes.ReportesDeCatalogos.Rep1Dis;
+import com.example.loginapp.Reportes.ReportesDeCatalogos.Rep1Pro;
 import com.example.loginapp.Reportes.ReportesDeCatalogos.Rep1VDis;
 import com.example.loginapp.databinding.ActivityIndexRepoCataBinding;
 
 public class IndexRepoCata extends DrawerBaseActivity {
     ActivityIndexRepoCataBinding aircb;
-    CardView CAT,DIS,VDIS;
+    CardView CAT,DIS,VDIS,PRO;
     Intent i;
 
     @Override
@@ -32,6 +33,7 @@ public class IndexRepoCata extends DrawerBaseActivity {
         CAT=findViewById(R.id.T1);
         DIS=findViewById(R.id.T2);
         VDIS=findViewById(R.id.T3);
+        PRO=findViewById(R.id.T4);
         CAT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,6 +52,13 @@ public class IndexRepoCata extends DrawerBaseActivity {
             @Override
             public void onClick(View v) {
                 i=new Intent(IndexRepoCata.this, Rep1VDis.class);
+                startActivity(i);
+            }
+        });
+        PRO.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i=new Intent(IndexRepoCata.this, Rep1Pro.class);
                 startActivity(i);
             }
         });
