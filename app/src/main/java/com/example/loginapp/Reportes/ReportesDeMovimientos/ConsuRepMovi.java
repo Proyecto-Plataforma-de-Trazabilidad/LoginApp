1e1fa531e37cdbf713fc53b385fa73074c98bd30
nayleaA <naygdz306@gmail.com>
2023-05-17 12:35:14 -0600

    TQ y Te productor

--- app/src/main/java/com/example/loginapp/Reportes/ReportesDeMovimientos/ConsuRepMovi.java
+++ app/src/main/java/com/example/loginapp/Reportes/ReportesDeMovimientos/ConsuRepMovi.java
@@ -7,11 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.loginapp.R;
-import com.example.loginapp.Reportes.IndexReportes;
 import com.google.android.material.button.MaterialButton;
 
 public class ConsuRepMovi extends AppCompatActivity {
-    MaterialButton TPP;
+    MaterialButton TPP,TQP,TEP;
     Intent i;
 
     @Override
@@ -20,10 +19,26 @@ public class ConsuRepMovi extends AppCompatActivity {
         setContentView(R.layout.activity_consu_rep_movi);
 
         TPP=findViewById(R.id.btn1);
+        TQP=findViewById(R.id.btn2);
+        TEP=findViewById(R.id.btn3);
         TPP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 i=new Intent(ConsuRepMovi.this, RepTPP.class);
+                startActivity(i);
+            }
+        });
+        TQP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i=new Intent(ConsuRepMovi.this, RepTQP.class);
+                startActivity(i);
+            }
+        });
+        TEP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i=new Intent(ConsuRepMovi.this, RepTEP.class);
                 startActivity(i);
             }
         });
